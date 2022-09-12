5315f001563914cf2c89ee562c317282a891d6ae
cybirddude <113386924+cybirddude@users.noreply.github.com>
2022-09-12 15:43:21 -0500

    Update StringMix.java

--- StringMix.java
+++ StringMix.java
@@ -15,18 +15,16 @@ public class Main
 	       System.out.println("Enter a string that contains only letters and is no less than 3, and no greater ");
 	       System.out.println("than 10");
 	       String S = sc.nextLine(); //The string the user will enter.
-	       if (S.length() < 3 || S.length() > 10) 
+	       if (S.length() > 3 || S.length() < 10) //Checking to see if the string length is between 3 and 10, inclusive.
 	       {
-		  for(int i = 0; i < S.length(); i++)
-		  {
-			if (Character.isletter(S[i]) == false)
-			{
-	       		System.out.println("Sorry, that string is not valid")
-			break;
-			}
-		  }
+		       if(S.matches("[a-zA-Z]+") == false) //Checking if string contains only letter characters.
+			       System.out.println("Sorry, that string is not valid");
 	       }
+	       else 
+		       System.out.println("Sorry, that string is not valid"); // if string does not between 3 and 10, inclusive
 	    }
-	    while(S.length() < 3 || 10 < S.length()); // The string cannot be greater than 10, less than 3, or contain a non-letter character.
+	    while(S.length() < 3 || 10 < S.length() || S.matches("[a-zA-Z]+") == false); // The string cannot be greater than 10, less than 3, 
+											 //or contain a non-letter character.
+	   
 	}
 }
