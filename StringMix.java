09bdaf9a09396210846e8fc3a031eeae344be78e
cybirddude <113386924+cybirddude@users.noreply.github.com>
2022-09-12 23:20:16 -0500

    Update StringMix.java

--- StringMix.java
+++ StringMix.java
@@ -7,6 +7,7 @@ import java.util.*;
 public class Main
 {
 	public static void main(String[] args) {
+	    String [] two, three, four, five, six, seven, eight, nine, ten; //Arrays containing strings of specific lengths.
 	    String alphafile = "words_alpha.txt"; // The name of the file.
 	    File f = new File(alphafile); //The actual file.
 	    Scanner sc= new Scanner(System.in);
@@ -25,6 +26,29 @@ public class Main
 	    }
 	    while(S.length() < 3 || 10 < S.length() || S.matches("[a-zA-Z]+") == false); // The string cannot be greater than 10, less than 3, 
 											 //or contain a non-letter character.
-	   
+	  String s = S.toLowerCase(); //Converts any uppercase letters to lowercase ones.
+	  Scanner scanner = new Scanner(f); //This is the scanner used to check the lines of the file.
+          while (scanner.hasNextLine()) //This will read every line of the file.
+	  {
+		  String data = scanner.nextLine(); //Data string is used to check individual lines in a file.
+		  boolean valid = false; //checks if a word in the file is contained in the string.
+		  if(data.matches("[a-zA-Z]+") == true && data.length() <= s.length()) //checking if line in a file matches standards of S.
+		  {
+			  for(int i; i < data.length(); i++)
+				  if(s[i] == data[0]) //checking to see if the first character of a line in a file is found in S
+				     {
+					  for(int j; j < s.length(); j++)
+					  {
+						if(s.contains(data[j])) // checking if a letter is found in the string.
+						{
+							
+						}
+					 }
+				     }
+				  
+		  }
+		  
+	  }
+	scanner.close(); //closes the file.
 	}
 }
