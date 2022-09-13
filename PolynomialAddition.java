50b24e060e7a4367e4e4226476142164225f7600
cybirddude <113386924+cybirddude@users.noreply.github.com>
2022-09-13 11:46:00 -0500

    Update PolynomialAddition.java

--- PolynomialAddition.java
+++ PolynomialAddition.java
@@ -1,6 +1,18 @@
+//Name:Eddie Lemmon
+//Course:CMP-SCI-3130
+//Due Date: October 2, 2022
+//Purpose of program: To compare the speed of three methods of comparing polynomials.
+import java.util.*;
 public class Main
-{
+{       static void hardway(double x, int k)//Uses direct multiplication to evaluate polynomials.
+                                               {
+
+        }
+        static void mediumway(double x, int k)//Uses the power function to evaluate polynomials.
+        static void easyway(double x, int k) //Uses horner's method to evaluate polynomials.
 	public static void main(String[] args) {
-		
+                int degree = 200000;//Degree of polynomial
+                double x;//Every coefficient will be multiplied by x.
+		int[] c = new int[degree];//array that will hold coefficient.
 	}
 }
