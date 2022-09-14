db5c2f95c1d0075cd88e6862c482cc2cda9d8bb4
cybirddude <113386924+cybirddude@users.noreply.github.com>
2022-09-14 00:14:49 -0500

    Update PolynomialAddition.java

--- PolynomialAddition.java
+++ PolynomialAddition.java
@@ -4,12 +4,14 @@
 //Purpose of program: To compare the speed of three methods of comparing polynomials.
 import java.util.*;
 public class Main
-{       static void hardway(double x, int k)//Uses direct multiplication to evaluate polynomials.
+{       static void hardway(double poly[], int x, int k)//Uses direct multiplication to evaluate polynomials.
                                                {
 
         }
-        static void mediumway(double x, int k)//Uses the power function to evaluate polynomials.
-        static void easyway(double x, int k) //Uses horner's method to evaluate polynomials.
+        static void mediumway(double poly[], int x, int k)//Uses the power function to evaluate polynomials.
+	//Uses horner's method to evaluate polynomials.
+        //Modified version of another method: https://www.geeksforgeeks.org/horners-method-polynomial-evaluation/
+        static void easyway(double poly[], int x, int k)
 	public static void main(String[] args) {
                 int degree = 200000;//Degree of polynomial
                 double x;//Every coefficient will be multiplied by x.
