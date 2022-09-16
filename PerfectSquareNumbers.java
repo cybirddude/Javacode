04ae778e9ba59bb398893c1dbbca40f252fb4f21
cybirddude <113386924+cybirddude@users.noreply.github.com>
2022-09-16 18:52:59 -0500

    Update PerfectSquareNumbers.java

--- PerfectSquareNumbers.java
+++ PerfectSquareNumbers.java
@@ -1,1 +1,21 @@
+public class Main
+  
+  import java.lang.*;
+  import java.util.*;
+{
+  
+	public static void main(String[] args) {
+    int x;
+    int a;
+    int b;
+    double r = (Math.pow(a, 2) + Math.pow(b, 2))/((a*b) + 1));
+    Scanner sc 
+    do
+    {
+      System.out.println(
+    
+    }while(x < 1 || x > 100000);
+      
+	}
+}
 
