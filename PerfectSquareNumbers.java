8cdc2371c865721da450875b07e731be95f47a11
cybirddude <113386924+cybirddude@users.noreply.github.com>
2022-09-23 09:58:13 -0500

    Update PerfectSquareNumbers.java

--- PerfectSquareNumbers.java
+++ PerfectSquareNumbers.java
@@ -21,10 +21,11 @@ return ((sqrt - Math.floor(sqrt)) == 0);
     double x; //Will be the number that the user enters.
     double a = 0; //Will be one of the variables
     double b = 0; //Will be the second variable.
-    double r; //The formula in a single variable.
+    double r; //The formula in a single variable. It is important that the formula is not defined yet, in order to prevent complications later.
     int total_int = 0; //Counts the number of integers.
     int perfect = 0; //Counts the number of perfect squares.
     Scanner sc = new Scanner(System.in);
+    do
     {
       System.out.println("Please enter an integer between 1 and 100000 (inclusive),");
       System.out.println("I will test values of the integers a and b to see which results of the formula r = (a^2 + b^2)/((a*b) + 1)) are integers");
@@ -68,5 +69,43 @@ return ((sqrt - Math.floor(sqrt)) == 0);
 		      
 	      }
       }
+		//Second part of the program will perform the same tasks, only it will display the time it took to do the work instead of the math data.
+		//reseting perfect, a, b, and total_int to 0 in order to use them again for the next part of the program.
+		perfect = 0; 
+		a = 0;
+		b = 0;
+		total_int = 0;
+		long start = currentTimeMillis(); //Start of the timer
+	 for(int i = 0; i < x; i++) //Second recusive function is like the first, only without displaying information.
+      {
+              b = 0; //This is necessary in order to reset b after the second loop finishes.
+	      a++; //This is necessary so that all of the numbers will be tested and so a will never equal 0.
+	      for(int j = 0; j < x; j++) //Second for loop adds a squared to all values of b squared to test the loop.
+	      {
+		      b++;
+		      r = (Math.pow(a, 2) + Math.pow(b, 2))/((a*b) + 1); //The formula is defined in the loop to prevent errors.
+		      if(r == (int)r)
+	         { 
+	          total_int++;
+		     		 if(checkPerfectSquare(r))
+		     	 {
+			    	        perfect++;
+					System.out.println("a = " + a);
+					System.out.println("b = " + b);
+					System.out.println("r = " + r);
+					
+		     	 }
+         	 }			 
+		      
+	      }
+      }
+		long end = currentTimeMillis(); //End of the timer
+		long middle = end - start; //The time it took to perform the work in the for loop
+		//Final five lines are a summary of the loops
+		System.out.println("The largest number for a and b was " + x);
+		System.out.println("The number of pairs tested was " + (x * x));
+		System.out.println("There were " + total_int + " integers in total.");
+		System.out.println("There were " + perfect + "perfect squares in total.");
+		System.out.println("It took " + middle "ms to do the work.");
 	}
 }
