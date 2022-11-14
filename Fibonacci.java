72dc189748fed9148d5ae9336bf6eaa3ee32b63e
cybirddude <113386924+cybirddude@users.noreply.github.com>
2022-11-14 04:57:46 -0600

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -13,6 +13,29 @@ public class Main
     if (n <= 1) return 1;
     return rfibo(n - 1) + rfibo(n - 2);
   }
+  public static long maxformula()
+  {
+    int gr = (1 + sqrt(5)) / 2; //Golden ratio formula
+    int n = 0;
+    long fibo;
+    while(fibo > 0;) //While loop runs while
+    {
+      fibo = (1 / sqrt(5)) * (Math.pow(gr, n)) - (Math.pow(-1, n)/Math.pow(gr, n)); //Fibonacci number formula using golden ratio.
+        n++;
+    }
+    return n;
+  }
+  public static long maxoverflow()
+    {
+       int n = 0;
+       long fibo;
+       while(fibo > 0 || fibo < Long.MAX_VALUE) //While loop runs while fibo is positive and less than the maximum long value.
+       {
+         fibo = rfibo(n);
+         n++;
+       }
+    return n;
+    }
   public static BigInteger afibo(int n)
   {
     int[] F = new int[n];
