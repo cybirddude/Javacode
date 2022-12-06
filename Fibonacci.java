0567986ef097c46c41b383d4e3d1092060ccc184
cybirddude <113386924+cybirddude@users.noreply.github.com>
2022-12-06 14:22:46 -0600

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -4,46 +4,23 @@
 //Purpose of Program:Compare the speed of three methods of finding fibonacci numbers.
 import java.util.*;
 import java.io.*;
-import java.lang.*;
+import java.math.BigInteger;
 public class Main
 {
-  public static long rfibo(int n)
+  public static long rfibo(long n)
   {
     if (n <= 0) return 0;
     if (n <= 1) return 1;
     return rfibo(n - 1) + rfibo(n - 2);
   }
-  public static long maxformula()
+  public static BigInteger afibo(int n)
   {
-    long gr = (1 + sqrt(5)) / 2; //Golden ratio formula
-    long n = 0;
-    long fibo = 0;
-    while(fibo >= 0;) //While loop runs while
-    {
-      fibo = (1 / sqrt(5)) * (Math.pow(gr, n)) - (Math.pow(-1, n)/Math.pow(gr, n)); //Fibonacci number formula using golden ratio.
-        n++;
-    }
-    return n;
-  }
-  public static long maxoverflow()
-    {
-       long n = 0;
-       long fibo = 0;
-       while(fibo >= 0 && fibo < Long.MAX_VALUE) //While loop runs while fibo is positive and less than the maximum long value.
-       {
-         fibo = rfibo(n);
-         n++;
-       }
-    return n;
-    }
-  public static BigInteger afibo(BigInteger n)
-  {
-    BigInteger[] F = new BigInteger[n];
+    BigInteger F[] = new BigInteger[n + 2];
     F[0] = BigInteger.ZERO;
     F[1] = BigInteger.ONE;
-    for(i = 2; i <= n; i++)
-      F[i] = F[i-1].add(F[i-2]);
-    return F[n - 1];
+    for(int i = 2; i <= n; i++)
+        F[i] = F[i-1].add(F[i-2]);
+    return F[n];
   }
   //Next four methods are related to the extra credit method.
   public static BigInteger exfig(BigInteger n)
@@ -84,13 +61,24 @@ public class Main
     if(n.equals(BigInteger.ZERO) || n.equals(BigInteger.ONE))
       return;
     BigInteger M[][] = new BigInteger[][]{{BigInteger.ONE, BigInteger.ONE}, {BigInteger.ONE, BigInteger.ZERO}};
-    squaring(F, n.shiftright(1));
+    squaring(F, n.shiftRight(1));
     multiply(F,F);
-    if(n.mod(BigInteger.valueof(2)).equals(BigInteger.ONE))
+    if(n.mod(BigInteger.valueOf(2)).equals(BigInteger.ONE))
       multiply(F, M);
   }
     
   public static void main(String[] args){
     long n;
+    Scanner sc = new Scanner(System.in);
+    do
+    {
+        n = sc.nextLong();
+    }while(n < 0 || n > 94);
+    long go = System.currentTimeMillis();
+    System.out.println("The fibonacci number for " + n + " using the recursive algorithm is:" + rfibo(n));
+    long stop = System.currentTimeMillis();
+    float middle = (stop - go)/1000F;
+    System.out.println("It took + " + middle + " seconds using the recursive method.");
+    System.out.println("The fibonacci number for " + n + " using the iterative algorithm is:" + afibo((int) n));
   }
 }
