d81e39418ef7788a1caa752d2b727b55cdce8e90
cybirddude <113386924+cybirddude@users.noreply.github.com>
2022-12-09 00:27:26 -0600

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,3 +1,4 @@
+
 //Name: Eddie Lemmon
 //Course: CMP-SCI-3130
 //Due Date: December 11, 2022
@@ -39,8 +40,8 @@ public class Main
   public static void multiply(BigInteger F[][], BigInteger M[][]) //Alternate version of the BigInteger's multiply method designed for matrices.
   {
     BigInteger x = (F[0][0].multiply(M[0][0])).add(F[0][1].multiply(M[1][0]));
-    BigInteger y = (F[0][0].multiply(M[0][0])).add(F[1][1].multiply(M[1][0]));
-    BigInteger z = (F[1][0].multiply(M[0][0])).add(F[0][1].multiply(M[1][0]));
+    BigInteger y = (F[0][0].multiply(M[0][1])).add(F[0][1].multiply(M[1][1]));
+    BigInteger z = (F[1][0].multiply(M[0][0])).add(F[1][1].multiply(M[1][0]));
     BigInteger w = (F[1][0].multiply(M[0][1])).add(F[1][1].multiply(M[1][1]));
     
     F[0][0] = x;
@@ -70,15 +71,31 @@ public class Main
   public static void main(String[] args){
     long n;
     Scanner sc = new Scanner(System.in);
+    System.out.println("In this program, I am going to output a fibonacci number based on your input using three different algorithms.");
+    System.out.println("From slowest to fastest, the three algorithms are recurssive, iterative, and logarithmic.");
+    System.out.println("To prove it, I will show how much time it took for each algorithm.");
+    System.out.println("The differences in speed are negligible at low inputs, higher inputs give more meaningful differnces.");
+    System.out.println("According to the LongOverflow.java program, the maximum long variable that outputs a fibonacci variable is 93.");
     do
     {
-        n = sc.nextLong();
+        System.out.println("Enter a whole number between 1 and 93, inclusive:");
+        n = (long) sc.nextLong();
     }while(n < 0 || n > 94);
     long go = System.currentTimeMillis();
     System.out.println("The fibonacci number for " + n + " using the recursive algorithm is:" + rfibo(n));
     long stop = System.currentTimeMillis();
     float middle = (stop - go)/1000F;
-    System.out.println("It took + " + middle + " seconds using the recursive method.");
+    System.out.println("It took " + middle + " seconds using the recursive method.");
+    System.out.println("(Fun fact: it takes the recussive method around half a minute to calculate using an input of 48 or 49. )");
+    go = System.currentTimeMillis();
     System.out.println("The fibonacci number for " + n + " using the iterative algorithm is:" + afibo((int) n));
+    stop = System.currentTimeMillis();
+    middle = (stop - go)/1000F;
+    System.out.println("It took " + middle + " seconds using the iterative method.");
+    go = System.currentTimeMillis();
+    System.out.println("The fibonacci number for " + n + " using the logarithmic algorithm is " + exfig(BigInteger.valueOf(n)));
+    stop = System.currentTimeMillis();
+    middle = (stop - go)/1000F;
+    System.out.println("It took " + middle + "seconds using the logarithmic method.");
   }
 }
